71dc32a2604fb435b480bf744e646f228da52797
neto test <test@neto.com>
2025-09-24 20:31:13 +0300

    Изменения в задании1

--- main.py
+++ main.py
@@ -2,32 +2,33 @@
 
 with open('recipes.txt', encoding='utf-8') as file:
     cook_book = {}
-    for i in file:
-        recipe_name = i.strip()
-        ingredients_count = file.readline()
+    for line in file:
+        recipe_name = line.strip()
+        ingredients_count = int(file.readline().strip())
         ingredients = []
-        for p in range(int(ingredients_count)):
-            recipe = file.readline().strip().split(' | ')
-            product, quantity, word = recipe
-            ingredients.append({'product': product, 'quantity': int(quantity), 'measure': word})
-        file.readline()
+        for _ in range(ingredients_count):
+            product, quantity, measure = file.readline().strip().split(' | ')
+            ingredients.append({
+                'product': product,
+                'quantity': int(quantity),
+                'measure': measure
+            })
+        file.readline()  # пропускаем пустую строку
         cook_book[recipe_name] = ingredients
 
 print('\nКУЛИНАРНАЯ КНИГА, СЛОВАРЬ К ЗАДАНИЮ 1')
 print('{')
 for recipe_name, ingredients in cook_book.items():
     print(f"  '{recipe_name}': [")
-    for i, ingredient in enumerate(ingredients):
-        if i == len(ingredients) - 1:  # последний элемент
-            print(
-                f"    {{'product': '{ingredient['product']}', 'quantity': {ingredient['quantity']}, 'measure': '{ingredient['measure']}'}}")
+    for ingredient in ingredients:
+        print(
+            f"    {{'product': '{ingredient['product']}', 'quantity': {ingredient['quantity']}, 'measure': '{ingredient['measure']}'}}",
+            end='')
+        if ingredient != ingredients[-1]:  # добавляем запятую если не последний элемент
+            print(',')
         else:
-            print(
-                f"    {{'product': '{ingredient['product']}', 'quantity': {ingredient['quantity']}, 'measure': '{ingredient['measure']}'}},")
-    if recipe_name == list(cook_book.keys())[-1]:  # последний рецепт
-        print("  ]")
-    else:
-        print("  ],")
+            print()
+    print("  ]" + ("," if recipe_name != list(cook_book.keys())[-1] else ""))
 print('}')
 
 
